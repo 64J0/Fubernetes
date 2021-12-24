9787dd55a22fbe2371b742ec006d7475a086105d
64J0 <vinigaio97@gmail.com>
2021-12-24 15:31:30 -0300

    feat: why this thing does not work? wtf

---
+++ Fsharp-K8s.Test/Service.fs
@@ -0,0 +1,11 @@
+﻿open Expecto
+
+let tests =
+  test "A simple test" {
+    let subject = "Hello World"
+    Expect.equal subject "Hello World" "The strings should equal"
+  }
+
+[<EntryPoint>]
+let main args =
+  runTestsWithCLIArgs [] args tests
