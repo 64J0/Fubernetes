32cce31879c6b8e02be548ba6fecc47430576fe2
64J0 <vinigaio97@gmail.com>
2023-08-15 18:29:26 -0300

    feat: lint the project, add pr template and lint workflow

---
+++ .github/pull_request_template.md
@@ -0,0 +1,7 @@
+## Description:
+
+<!-- Explain what you're changing, presenting the necessary details to understand. -->
+
+## How to test it?
+
+<!-- Add instructions on how to test this PR locally. -->
--- Fubernetes.Test/Program.fs
+++ Fubernetes.Test/Program.fs
@@ -2,12 +2,8 @@ open Expecto
 open Tests.ConfigMap
 open Tests.Service
 
-let projectTests =
-  testList "k8s" [
-    configMapTestsList
-    serviceTestsList
-  ]
+let projectTests = testList "k8s" [ configMapTestsList; serviceTestsList ]
 
 [<EntryPoint>]
 let main args =
-  runTestsWithCLIArgs [] args projectTests
+    runTestsWithCLIArgs [] args projectTests
