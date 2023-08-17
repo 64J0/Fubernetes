78e1dacf5b0993f09b08d73d2800cc8bd78a6f06
64J0 <vinigaio97@gmail.com>
2023-08-17 15:56:01 -0300

    refac: finish the refactoring of ConfigMap

--- Fubernetes.Main/src/ConfigMap.fs
+++ Fubernetes.Main/src/ConfigMap.fs
@@ -1,104 +1,37 @@
-namespace Fubernetes.Resources
-
-open System
-open System.IO
+namespace Fubernetes
 
 open YamlDotNet.Serialization
 open YamlDotNet.Serialization.NamingConventions
 
+type Metadata = { Name: string; Namespace: string }
+
+type ConfigMapConstructor =
+    { ApiVersion: string
+      Kind: string
+      Metadata: Metadata
+      Data: Map<string, string>
+      BinaryData: Map<string, string>
+      Immutable: bool }
+
+    static member Default =
+        { ApiVersion = "v1"
+          Kind = "ConfigMap"
+          Metadata =
+            { Name = "default-configmap"
+              Namespace = "default" }
+          Data = Map.empty
+          BinaryData = Map.empty
+          Immutable = false }
+
 // https://kubernetes.io/docs/concepts/configuration/configmap/
-module ConfigMap =
-    type ConfigMapConstructor =
-        { Name: string // should be lowercase
-          Namespace: string
-          Data: List<Shared.TupleString>
-          BinaryData: List<Shared.TupleString>
-          Immutable: bool }
+type ConfigMap(constructor: ConfigMapConstructor) =
+    member private this.getSerializer() : ISerializer =
+        SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build()
 
-    type ConfigMap(constructor: ConfigMapConstructor) =
-        member private this.addName(templateString: string) =
-            let nameId = "$NAME$"
-            templateString.Replace(nameId, constructor.Name)
+    member this.toYamlBuffer() =
 
-        member private this.addNamespace(templateString: string) =
-            let namespaceId = "$NAMESPACE$"
-            templateString.Replace(namespaceId, constructor.Namespace)
+        let serializer = this.getSerializer ()
 
-        // https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/strings
-        member private this.getTupleString(tuple: Shared.TupleString) = $"\n\t{fst tuple}: {snd tuple}"
-
-        member private this.addData(templateString: string) =
-            let dataId = "$DATA$"
-
-            let dataValue =
-                constructor.Data |> List.map this.getTupleString |> Shared.reduceIfNotEmpty (+)
-
-            templateString.Replace(dataId, dataValue)
-
-        member private this.addBinaryData(templateString: string) =
-            let binaryDataId = "$BINARY_DATA$"
-
-            let binaryDataValue =
-                constructor.BinaryData
-                |> List.map this.getTupleString
-                |> Shared.reduceIfNotEmpty (+)
-
-            templateString.Replace(binaryDataId, binaryDataValue)
-
-        member private this.addImmutable(templateString: string) =
-            let immutableId = "$IMMUTABLE$"
-
-            let immutableValue =
-                match constructor.Immutable with
-                | true -> "true"
-                | false -> "false"
-
-            templateString.Replace(immutableId, immutableValue)
-
-        member this.toYamlBuffer() =
-            let templatePath = Shared.getTemplatesDirPath "/configmap/ConfigMap.template"
-
-            File.ReadAllText(templatePath, Text.Encoding.UTF8)
-            |> this.addName
-            |> this.addNamespace
-            |> this.addData
-            |> this.addBinaryData
-            |> this.addImmutable
-            |> Shared.replaceTabsWithSpaces
-            |> Shared.removeEmptyLines
-
-module ConfigMapTest =
-
-    type Metadata = { Name: string; Namespace: string }
-
-    type ConfigMapConstructor =
-        { ApiVersion: string
-          Kind: string
-          Metadata: Metadata
-          Data: Map<string, string>
-          BinaryData: Map<string, string>
-          Immutable: bool }
-
-        static member Default =
-            { ApiVersion = "v1"
-              Kind = "ConfigMap"
-              Metadata =
-                { Name = "default-configmap"
-                  Namespace = "default" }
-              Data = Map.empty
-              BinaryData = Map.empty
-              Immutable = false }
-
-    type ConfigMap(constructor: ConfigMapConstructor) =
-        member private this.getSerializer() : ISerializer =
-            SerializerBuilder()
-                .WithNamingConvention(CamelCaseNamingConvention.Instance)
-                .Build()
-
-        member this.toYamlBuffer() =
-
-            let serializer = this.getSerializer ()
-
-            let serializedConfigMap = serializer.Serialize constructor
-
-            printfn "Serialized ConfigMap: %A" serializedConfigMap
+        serializer.Serialize constructor
